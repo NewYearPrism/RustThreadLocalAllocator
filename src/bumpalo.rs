9cc3a574112c8594fc14437e2e232b9ec0ba7dcb
NewYearPrism <NewYearPrism@outlook.com>
2025-11-23 15:25:34 +0800

    mutability

--- src/bumpalo.rs
+++ src/bumpalo.rs
@@ -1,4 +1,5 @@
 use std::alloc::Layout;
+use std::cell::RefCell;
 use std::ptr::NonNull;
 
 use allocator_api2::alloc::AllocError;
@@ -10,21 +11,21 @@ pub struct ThreadLocalBump;
 
 impl ThreadLocalBump {
     thread_local! {
-        pub static BUMP: Bump = Bump::new();
+        pub static BUMP: RefCell<Bump> = RefCell::new(Bump::new());
     }
 }
 
 unsafe impl Allocator for ThreadLocalBump {
     fn allocate(&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
-        Self::BUMP.with(|bump| bump.allocate(layout))
+        Self::BUMP.with(|bump| (&*bump.borrow()).allocate(layout))
     }
 
     unsafe fn deallocate(&self, ptr: NonNull<u8>, layout: Layout) {
-        Self::BUMP.with(|bump| unsafe { bump.deallocate(ptr, layout) })
+        Self::BUMP.with(|bump| unsafe { (&*bump.borrow()).deallocate(ptr, layout) })
     }
 
     fn allocate_zeroed(&self, layout: std::alloc::Layout) -> Result<NonNull<[u8]>, AllocError> {
-        Self::BUMP.with(|bump| bump.allocate_zeroed(layout))
+        Self::BUMP.with(|bump| (&*bump.borrow()).allocate_zeroed(layout))
     }
 
     unsafe fn grow(
@@ -33,7 +34,7 @@ unsafe impl Allocator for ThreadLocalBump {
         old_layout: Layout,
         new_layout: Layout,
     ) -> Result<NonNull<[u8]>, AllocError> {
-        Self::BUMP.with(|bump| unsafe { bump.grow(ptr, old_layout, new_layout) })
+        Self::BUMP.with(|bump| unsafe { (&*bump.borrow()).grow(ptr, old_layout, new_layout) })
     }
 
     unsafe fn grow_zeroed(
@@ -42,7 +43,7 @@ unsafe impl Allocator for ThreadLocalBump {
         old_layout: Layout,
         new_layout: Layout,
     ) -> Result<NonNull<[u8]>, AllocError> {
-        Self::BUMP.with(|bump| unsafe { bump.grow_zeroed(ptr, old_layout, new_layout) })
+        Self::BUMP.with(|bump| unsafe { (&*bump.borrow()).grow_zeroed(ptr, old_layout, new_layout) })
     }
 
     unsafe fn shrink(
@@ -51,6 +52,6 @@ unsafe impl Allocator for ThreadLocalBump {
         old_layout: Layout,
         new_layout: Layout,
     ) -> Result<NonNull<[u8]>, AllocError> {
-        Self::BUMP.with(|bump| unsafe { bump.shrink(ptr, old_layout, new_layout) })
+        Self::BUMP.with(|bump| unsafe { (&*bump.borrow()).shrink(ptr, old_layout, new_layout) })
     }
 }
